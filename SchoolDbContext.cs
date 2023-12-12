8a8575f9194d2f085cb9df8430ea5d99ad8ad158
ABDUL REHMAN <152426209+future-afternoon@users.noreply.github.com>
2023-12-12 15:44:36 +0500

    Add files via upload

---
+++ SchoolDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class SchoolDbContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Faculty> Faculties { get; set; }
+    public DbSet<Class> Classes { get; set; }
+    public DbSet<Enrolled> Enrollments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=school.db");
+    }
+}
